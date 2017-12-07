12d841c5007f2054fe89767616dd3782251871b7
Lynn Bradshaw <readyready15728@gmail.com>
2017-12-07 07:58:27 -0500

    Adding controllers to app_api

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const locationModel = mongoose.model('location');
+
+module.exports.locationsByDistance = function (req, res) {};
+module.exports.createLocation = function (req, res) {};
+module.exports.readLocation = function (req, res) {};
+module.exports.updateLocation = function (req, res) {};
+module.exports.deleteLocation = function (req, res) {};
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,6 @@
+module.exports.createReview = function (req, res) {};
+module.exports.readReview = function (req, res) {};
+module.exports.updateReview = function (req, res) {};
+module.exports.deleteReview = function (req, res) {};
+
+module.exports = router;
