e1cb54697b740628faa89a2c5bf3c95577cff507
Lynn Bradshaw <readyready15728@gmail.com>
2017-12-13 08:26:23 -0500

    Adding review creation to app_api

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,9 +1,88 @@
 const mongoose = require('mongoose');
 const Location = mongoose.model('location');
 
+const doAddReview = function (req, res, location) {
+  if (!location) {
+    res.status(404);
+    res.json({message: 'locationId not found'});
+  } else {
+    location.reviews.push({
+      author: req.body.author,
+      rating: req.body.rating,
+      reviewText: req.body.reviewText,
+    });
+
+    location.save(function (err, location) {
+      let thisReview;
+
+      if (err) {
+        res.status(400);
+        res.json(err);
+      } else {
+        updateAverageRating(location._id);
+        thisReview = location.reviews[location.reviews.length - 1];
+
+        console.log(thisReview);
+
+        res.status(201);
+        res.json(thisReview);
+      }
+    });
+  }
+};
+
+const updateAverageRating = function (locationId) {
+  Location
+    .findById(locationId)
+    .select('rating reviews')
+    .exec(function (err, location) {
+      if (!err) {
+        doSetAverageRating(location);
+      }
+    });
+};
+
+const doSetAverageRating = function (location) {
+  if (location.reviews && location.reviews.length > 0) {
+    const reviewCount = location.reviews.length;
+    let ratingTotal = 0;
+
+    for (let i = 0; i < reviewCount; i++) {
+      ratingTotal += location.reviews[i].rating;
+    }
+
+    const ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+    location.rating = ratingAverage;
+
+    location.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Average rating updated to', ratingAverage);
+      }
+    });
+  }
+};
+
 module.exports.createReview = function (req, res) {
-  res.status(200);
-  res.json({status: 'success'});
+  const locationId = req.params.locationId;
+
+  if (locationId) {
+    Location
+      .findById(locationId)
+      .select('reviews')
+      .exec(function (err, location) {
+        if (err) {
+          res.status(400);
+          res.json(err);
+        } else {
+          doAddReview(req, res, location);
+        }
+      });
+  } else {
+    res.status(404);
+    res.json({message: 'Not found; locationId required'});
+  }
 };
 
 module.exports.readReview = function (req, res) {
