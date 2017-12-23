2d0d7ffd9d036707411fe18bfb20888477ce2963
Lynn Bradshaw <readyready15728@gmail.com>
2017-12-23 06:20:01 -0500

    Setting up the beginnings of the loc8r AngularJS code

---
+++ public/angularjs/loc8r.js
@@ -0,0 +1,29 @@
+var locationListCtrl = function ($scope) {
+  $scope.data = {
+    locations: [{
+	    'name': 'Starcups',
+    	'address': '125 High Street, Reading, RG6 1PS',
+    	'rating': 3,
+      'facilities': ['Hot Drinks', 'Food', 'Premium Wi-Fi'],
+      'distance': 14.6
+      '_id': '5a27ab691c5e0a989c0634da'
+    }, {
+	    'name': 'Café Hero',
+    	'address': '125 High Street, Reading, RG6 1PS',
+    	'rating': 4,
+      'facilities': ['Hot Drinks', 'Food', 'Premium Wi-Fi'],
+      'distance': 14.6,
+      '_id': '5a2cdcc9877c0bf8cdf47355'
+    }, {
+	    'name': 'Burger Queen',
+    	'address': '125 High Street, Reading, RG6 1PS',
+    	'rating': 2,
+    	'facilities': ['Food', 'Premium Wi-Fi'],
+      '_id': '5a2ce5ae877c0bf8cdf47358'
+    }]
+  };
+};
+
+angular
+  .module('loc8r')
+  .controller('locationListCtrl', locationListCtrl);
