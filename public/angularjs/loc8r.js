404afbca621acd98cb346df04faba1801677c1ce
Lynn Bradshaw <readyready15728@gmail.com>
2017-12-24 12:17:33 -0500

    Adding distance formatting to Angular app home page

--- public/angularjs/loc8r.js
+++ public/angularjs/loc8r.js
@@ -1,3 +1,27 @@
+var _isNumeric = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+var formatDistance = function () {
+  return function (distance) {
+    var roundedDistance, unit;
+
+    if (distance && _isNumeric(distance)) {
+      if (distance > 1) {
+        roundedDistance = parseFloat(distance).toFixed(1);
+        unit = 'km';
+      } else {
+        roundedDistance = parseInt(distance * 1000, 10);
+        unit = 'm';
+      }
+
+      return roundedDistance + ' ' + unit;
+    } else {
+      return '?';
+    }
+  };
+};
+
 var locationListCtrl = function ($scope) {
   $scope.data = {
     locations: [{
@@ -27,4 +51,5 @@ var locationListCtrl = function ($scope) {
 
 angular
   .module('loc8r', [])
-  .controller('locationListCtrl', locationListCtrl);
+  .controller('locationListCtrl', locationListCtrl)
+  .filter('formatDistance', formatDistance);
