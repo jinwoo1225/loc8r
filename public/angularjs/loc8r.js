4f553142425b7e79b1cf8afffd971efea60b3688
Lynn Bradshaw <readyready15728@gmail.com>
2018-01-06 16:37:08 -0500

    Adding (intentionally crippled) geolocation capability to AngularJS app

--- public/angularjs/loc8r.js
+++ public/angularjs/loc8r.js
@@ -32,19 +32,60 @@ var ratingStars = function () {
 };
 
 var loc8rData = function ($http) {
-  return $http.get('/api/locations?lng=-0.79&lat=51.3&maxDistance=25');
+  return {
+    locationByCoords: function (lat, lng) {
+      return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=25');
+    }
+  };
 };
 
-var locationListCtrl = function ($scope, loc8rData) {
-  $scope.message = 'Searching for nearby places...';
+var geolocation = function () {
+  return {
+    getPosition: function (cbSuccess, cbError, cbNoGeo) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
+      }
+    }
+  };
+};
 
-  loc8rData.then(function (response) {
-    $scope.message = response.data.length > 0 ? '' : 'No locations found';
-    $scope.data = { locations: response.data };
-  }, function (err) {
-    $scope.message = 'Sorry, something\'s gone wrong.';
-    console.log(err);
-  });
+var locationListCtrl = function ($scope, loc8rData, geolocation) {
+  $scope.message = 'Checking your location...';
+
+  $scope.getData = function (position) {
+    // Geolocation intentionally crippled because the locations in the database
+    // are in the UK; I don't want to change them and announce where I live to
+    // the planet
+
+    /* var lat = position.coords.latitude;
+    var lng = position.coords.longitude; */
+    var lat = 51.3;
+    var lng = -0.79;
+
+    $scope.message = 'Searching for nearby places...';
+
+    loc8rData.locationByCoords(lat, lng).then(function (response) {
+      $scope.message = response.data.length > 0 ? '' : 'No locations found';
+      $scope.data = { locations: response.data };
+    }, function (err) {
+      $scope.message = 'Sorry, something\'s gone wrong.';
+      console.log(err);
+    });
+  };
+
+  $scope.showError = function (error) {
+    $scope.$apply(function () {
+      $scope.message = error.message;
+    });
+  };
+
+  $scope.noGeo = function () {
+    $scope.$apply(function () {
+      $scope.message = 'Geolocation not supported by this browser.';
+    });
+  };
+
+  geolocation.getPosition($scope.getData, $scope.showError, $scope.noGeo);
 };
 
 angular
@@ -52,4 +93,5 @@ angular
   .controller('locationListCtrl', locationListCtrl)
   .filter('formatDistance', formatDistance)
   .directive('ratingStars', ratingStars)
-  .service('loc8rData', loc8rData);
+  .service('loc8rData', loc8rData)
+  .service('geolocation', geolocation);
