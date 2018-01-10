aa103d640500d970e61c6cc30367390afed05708
Lynn Bradshaw <readyready15728@gmail.com>
2018-01-09 19:03:01 -0500

    Changing app_server routes to work with the new AngularJS SPA

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,13 +3,7 @@ const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
 
-// Locations pages
-router.get('/', ctrlLocations.homeList);
-router.get('/location/:locationId', ctrlLocations.locationInfo);
-router.get('/location/:locationId/reviews/new', ctrlLocations.addReviewGet);
-router.post('/location/:locationId/reviews/new', ctrlLocations.addReviewPost);
-
-// Other pages
+router.get('/', ctrlOthers.angularApp);
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
