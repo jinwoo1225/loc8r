373279e18b811d625f3439eec60f9945561c77e6
Lynn Bradshaw <readyready15728@gmail.com>
2018-01-10 08:23:37 -0500

    Adding directive ratingStars to AngularJS SPA

---
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -0,0 +1,13 @@
+var ratingStars = function () {
+  return {
+    restrict: 'EA',
+    scope: {
+      thisRating: '=rating'
+    },
+    templateUrl: '/common/directives/ratingStars/ratingStars.directive.html'
+  };
+};
+
+angular
+  .module('loc8r')
+  .directive('ratingStars', ratingStars);
