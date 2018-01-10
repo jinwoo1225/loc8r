fa6907869e2c36ae82e86880747ef5c575e9135f
Lynn Bradshaw <readyready15728@gmail.com>
2018-01-10 08:18:47 -0500

    Adding formatDistance filter to AngularJS SPA

---
+++ app_client/common/filters/formatDistance.filter.js
@@ -0,0 +1,27 @@
+var _isNumeric = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+var formatDistance = function () {
+  return function (distance) {
+    var roundedDistance, unit;
+
+    if (distance && _isNumeric(distance)) {
+      if (distance > 1) {
+        roundedDistance = parseFloat(distance).toFixed(1);
+        unit = 'km';
+      } else {
+        roundedDistance = parseInt(distance * 1000, 10);
+        unit = 'm';
+      }
+
+      return roundedDistance + ' ' + unit;
+    } else {
+      return '?';
+    }
+  };
+};
+
+angular
+  .module('loc8r')
+  .filter('formatDistance', formatDistance);
