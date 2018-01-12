a12a85900c8feb5bda476b09e4cb92b7a60a85e1
Lynn Bradshaw <readyready15728@gmail.com>
2018-01-11 21:34:36 -0500

    Confining AngularJS JavaScript components to IIFEs

--- app_client/common/services/geolocation.service.js
+++ app_client/common/services/geolocation.service.js
@@ -1,15 +1,17 @@
-var geolocation = function () {
-  return {
-    getPosition: function (cbSuccess, cbError, cbNoGeo) {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
-      } else {
-        cbNoGeo();
+(function () {
+  var geolocation = function () {
+    return {
+      getPosition: function (cbSuccess, cbError, cbNoGeo) {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
+        } else {
+          cbNoGeo();
+        }
       }
-    }
+    };
   };
-};
 
-angular
-  .module('loc8r')
-  .service('geolocation', geolocation);
+  angular
+    .module('loc8r')
+    .service('geolocation', geolocation);
+})();
