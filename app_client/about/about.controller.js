095355dcb7a54ccec80c6152a0af2b295fbdc1ca
Lynn Bradshaw <readyready15728@gmail.com>
2018-02-07 16:45:48 -0500

    Adding controller about to AngularJS SPA

---
+++ app_client/about/about.controller.js
@@ -0,0 +1,17 @@
+(function () {
+  var aboutCtrl = function () {
+    var vm = this;
+
+    vm.pageHeader = {
+      title: 'Loc8r'
+    };
+
+    vm.main = {
+      content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus. Lacrimosa, dies illa, qua resurget ex favilla judicandus homo reus.'
+    };
+  };
+
+  angular
+    .module('loc8r')
+    .controller('aboutCtrl', aboutCtrl);
+})();
