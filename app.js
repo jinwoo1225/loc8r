3b66b871b55ca19934ce71ddb8b0c27ea20faedb
Lynn Bradshaw <readyready15728@gmail.com>
2018-02-07 17:45:15 -0500

    Including AngularJS SPA controller about in minification step in app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set('view engine', 'jade');
 const appClientFiles = [
   'app_client/loc8r-spa.js',
   'app_client/home/home.controller.js',
+  'app_client/about/about.controller.js',
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
   'app_client/common/filters/formatDistance.filter.js',
