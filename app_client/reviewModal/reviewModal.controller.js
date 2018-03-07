49ea8ba239459dd6a193c64a1fdbf1912258c5a3
Lynn Bradshaw <readyready15728@gmail.com>
2018-03-06 23:21:03 -0500

    Making review submission functional in AngularJS SPA controller reviewModal

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -1,5 +1,5 @@
 (function () {
-  var reviewModalCtrl = function ($uibModalInstance, locationData) {
+  var reviewModalCtrl = function ($uibModalInstance, loc8rData, locationData) {
     var vm = this;
 
     $uibModalInstance.result.catch(function () { $uibModalInstance.close(); });
@@ -8,6 +8,29 @@
     vm.modal = {
       cancel: function () {
         $uibModalInstance.dismiss('cancel');
+      }
+    };
+    vm.addReview = function (locationData, formData) {
+      loc8rData.addReviewById(locationId, {
+        author: formData.name,
+        rating: formData.rating,
+        reviewText: formData.reviewText
+      }).then(function (data) {
+        console.log('Review submitted successfully');
+      }, function (err) {
+        vm.formError = 'Your review has not been saved; try again';
+      });
+
+      return false;
+    };
+    vm.onSubmit = function () {
+      vm.formError = ''; // reset any existing error message
+
+      if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
+        vm.formError = 'All fields are required; please try again';
+        return false;
+      } else {
+        vm.addReview(vm.locationData.locationId, vm.formData);
       }
     };
   };
