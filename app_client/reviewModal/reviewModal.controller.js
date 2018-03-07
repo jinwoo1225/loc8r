543013b093c83208a5cd69cba9f4745ce630c57f
Lynn Bradshaw <readyready15728@gmail.com>
2018-03-06 23:21:03 -0500

    Making review submission functional in app_client controller reviewModal

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -36,6 +36,29 @@
         vm.addReview(vm.locationData.locationId, vm.formData);
       }
     };
+    vm.addReview = function (locationData, formData) {
+      loc8rData.addReviewById(locationId, {
+        author: formData.name,
+        rating: formData.rating,
+        reviewText: formData.reviewText
+      }).then(function (data) {
+        console.log('Review submitted successfully');
+      }, function (err) {
+        vm.formError = 'Your review has not been saved; try again';
+      });
+
+      return false;
+    };
+    vm.onSubmit = function () {
+      vm.formError = ''; // reset any existing error message
+
+      if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
+        vm.formError = 'All fields are required; please try again';
+        return false;
+      } else {
+        vm.addReview(vm.locationData.locationId, vm.formData);
+      }
+    };
   };
 
 <<<<<<< HEAD
