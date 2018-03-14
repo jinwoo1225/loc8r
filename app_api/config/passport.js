4c3ab899a94fa11db8a4ea4f81101d3b05e2c09a
Lynn Bradshaw <readyready15728@gmail.com>
2018-03-14 10:03:55 -0400

    Adding config for passport to app_api

---
+++ app_api/config/passport.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = mongoose.model('User');
+
+passport.use(new LocalStrategy({usernameField: 'email'}), function (email, password, done) {
+  User.findOne({email: email}, function (err, user) {
+    if (err) {
+      return done(err);
+    }
+
+    if (!user) {
+      return done(null, false, {message: 'Incorrect username'});
+    }
+
+    if (!user.validPassword(password)) {
+      return done(null, false, {message: 'Incorrect password'});
+    }
+
+    return done(null, user);
+  });
+});
