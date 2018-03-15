5f7874449dc38cb8133ee4ef39d13c1bb40534d5
Lynn Bradshaw <readyready15728@gmail.com>
2018-03-14 22:23:36 -0400

    Using express-jwt in app_api routes for review functions

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const expressJwt = require('express-jwt');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlReviews = require('../controllers/reviews');
 const ctrlAuth = require('../controllers/authentication');
+
+const auth = expressJwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: 'payload'
+});
 
 router.get('/locations', ctrlLocations.locationsByDistance);
 router.post('/locations', ctrlLocations.createLocation);
@@ -10,10 +16,10 @@ router.get('/locations/:locationId', ctrlLocations.readLocation);
 router.put('/locations/:locationId', ctrlLocations.updateLocation);
 router.delete('/locations/:locationId', ctrlLocations.deleteLocation);
 
-router.post('/locations/:locationId/reviews', ctrlReviews.createReview);
+router.post('/locations/:locationId/reviews', auth, ctrlReviews.createReview);
 router.get('/locations/:locationId/reviews/:reviewId', ctrlReviews.readReview);
-router.put('/locations/:locationId/reviews/:reviewId', ctrlReviews.updateReview);
-router.delete('/locations/:locationId/reviews/:reviewId', ctrlReviews.deleteReview);
+router.put('/locations/:locationId/reviews/:reviewId', auth, ctrlReviews.updateReview);
+router.delete('/locations/:locationId/reviews/:reviewId', auth, ctrlReviews.deleteReview);
 
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
