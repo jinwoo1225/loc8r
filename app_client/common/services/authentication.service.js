bbc864687631cd780096dd84337d8ec37abd7da1
Lynn Bradshaw <readyready15728@gmail.com>
2018-03-19 21:39:07 -0400

    Completing AngularJS SPA service authentication

--- app_client/common/services/authentication.service.js
+++ app_client/common/services/authentication.service.js
@@ -1,11 +1,49 @@
 (function () {
   var authentication = function ($window) {
-    return {
-      saveToken: function (token) {
+    var saveToken = function (token) {
         $window.localStorage['loc8rToken'] = token;
+    };
+    
+    var getToken = function () {
+      return $window.localStorage['loc8rToken'];
+    };
+
+    return {
+      saveToken: saveToken,
+      getToken: getToken,
+      register: function (user) {
+        return $http.post('/api/register', user).then(function (response) {
+          saveToken(response.data.token);
+        };
       },
-      getToken: function () {
-        return $window.localStorage['loc8rToken'];
+      login: function (user) {
+        return $http.post('/api/login', user).then(function (response) {
+          saveToken(response.data.token);
+        };
+      },
+      logout: function () {
+        $window.localStorage.removeItem('loc8rToken');
+      },
+      isLoggedIn: function () {
+        var token = getToken();
+
+        if (token) {
+          var payload = JSON.parse($window.atob(token.split('.')[1]));
+          return payload.exp > Date.now() / 1000;
+        } else {
+          return false;
+        }
+      },
+      currentUser: function () {
+        if (isLoggedIn()) {
+          var token = getToken();
+          var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+          return {
+            email: payload.email,
+            name: payload.name
+          };
+        }
       }
     };
   };
