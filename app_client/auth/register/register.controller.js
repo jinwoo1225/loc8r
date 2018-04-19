de25e3b6c39260022244e2512f71852b8924430b
Lynn Bradshaw <readyready15728@gmail.com>
2018-04-19 03:21:42 -0400

    Adding controller register to AngularJS SPA

---
+++ app_client/auth/register/register.controller.js
@@ -0,0 +1,45 @@
+(function () {
+  angular
+    .module('loc8r')
+    .controller('registerCtrl', registerCtrl);
+
+  var registerCtrl = function ($location, authentication) {
+    var vm = this;
+
+    vm.pageHeader = {
+      title: 'Create a New Loc8r Account'
+    };
+
+    vm.credentials = {
+      name : '',
+      email : '',
+      password : ''
+    };
+
+    vm.returnPage = $location.search().page || '/';
+
+    vm.onSubmit = function () {
+      vm.formError = '';
+      if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
+        vm.formError = 'All fields are required; please try again';
+        return false;
+      } else {
+        vm.register();
+      }
+    };
+
+    vm.register = function() {
+      vm.formError = '';
+      authentication
+        .register(vm.credentials)
+        .then(function() {
+          $location.search('page', null); 
+          $location.path(vm.returnPage);
+        }, function(err) {
+          vm.formError = err;
+        });
+    };
+  };
+
+  registerCtrl.$inject = ['$location', 'authentication'];
+})();
