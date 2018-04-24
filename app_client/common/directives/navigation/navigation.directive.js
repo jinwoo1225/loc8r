823099e2e5da78e6b7887415898a57df821cf4cb
Lynn Bradshaw <readyready15728@gmail.com>
2018-04-23 20:40:44 -0400

    Including controller navigation in AngularJS SPA directive navigation

--- app_client/common/directives/navigation/navigation.directive.js
+++ app_client/common/directives/navigation/navigation.directive.js
@@ -2,7 +2,8 @@
   var navigation = function () {
     return {
       restrict: 'EA',
-      templateUrl: '/common/directives/navigation/navigation.template.html'
+      templateUrl: '/common/directives/navigation/navigation.template.html',
+      controller: 'navigationCtrl as navvm'
     };
   };
 
